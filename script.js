05d5a0db0140a7ff347784e4cd42165a3ee144be
ebenofere <eofere@gmail.com>
2022-04-26 16:21:01 +0100

    checkEmail()

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
+// Show input error message
 function showError(input, message) {
   const formControl = input.parentElement;
   formControl.className = "form-control error";
@@ -11,11 +12,13 @@ function showError(input, message) {
   small.innerText = message;
 }
 
+// show success outline
 function showSuccess(input) {
   const formControl = input.parentElement;
   formControl.className = "form-control success";
 }
 
+// Capitalise the first element of the field name
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
@@ -37,14 +40,21 @@ function checkLength(input, min, max) {
   }
 }
 
+//Check email
+const checkEmail = (input) => {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  re.test(input.value.trim())
+    ? showSuccess(input)
+    : showError(input, `Email is not valid`);
+};
+
 // Check passwords match
 function checkRequired(inputArr) {
   inputArr.forEach((input) => {
-    if (input.value.trim() === "") {
-      showError(input, `${getFieldName(input)} is required`);
-    } else {
-      showSuccess(input);
-    }
+    input.value.trim() === ""
+      ? showError(input, `${getFieldName(input)} is required`)
+      : showSuccess(input);
   });
 }
 
@@ -55,4 +65,5 @@ form.addEventListener("submit", function (e) {
   checkRequired([username, email, password, password2]);
   checkLength(username, 3, 15);
   checkLength(password, 6, 25);
+  checkEmail(email);
 });
